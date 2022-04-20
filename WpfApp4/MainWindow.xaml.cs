f43e2765457c393ec1ee319615bb391433d02295
student <student@K08-04>
2022-04-20 10:48:13 +0200

    Dodaj pliki projektów.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CoffieCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void TeaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void liliaBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void KonserwaPapiezBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void asterBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MangoShakeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
